d962db381782ecc62a3cc0abc787197ddbd7e034
Sara Acevedo <sara.g.acevedo@gmail.com>
2020-12-10 10:19:27 -0700

    hide indicators

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -28,7 +28,7 @@ import SignUpForm from "./SignUpForm";
 const Homepage = () => {
 
     // indicators used for development
-    const indicators = true;
+    const indicators = false;
 
     const { scrollY } = useViewportScroll();
 
